b7c00d0445179b3e20578a76aa548a67e2c8d8ac
samarjit-singh <samarjit@oatstech.co>
2023-11-09 22:04:11 +0530

    update post function

--- src/app/api/huggingface/route.ts
+++ src/app/api/huggingface/route.ts
@@ -1,29 +1,37 @@
 import { NextResponse } from "next/server";
+import { HfInference } from "@huggingface/inference";
+import { HuggingFaceStream, StreamingTextResponse } from "ai";
+import { experimental_buildOpenAssistantPrompt } from "ai/prompts";
 
-export const query = async (data: string) => {
+const Hf = new HfInference(process.env.TOKEN);
+
+export const runtime = "edge";
+
+export async function POST(req: Request) {
   const apiUrl =
     "https://api-inference.huggingface.co/models/OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5";
 
+  const { messages } = await req.json();
+
   try {
-    const response = await fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${process.env.TOKEN}`,
-        "Content-Type": "application/json",
+    const response = await Hf.textGenerationStream({
+      model: apiUrl,
+      inputs: experimental_buildOpenAssistantPrompt(messages),
+      parameters: {
+        max_new_tokens: 200,
+        // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
+        typical_p: 0.2,
+        repetition_penalty: 1,
+        truncate: 1000,
+        return_full_text: false,
       },
-      body: JSON.stringify(data),
     });
 
-    if (response.ok) {
-      const result = await response.json();
-      return result;
-    } else {
-      throw new Error(`API request failed with status: ${response.status}`);
-    }
+    const stream = HuggingFaceStream(response);
+
+    return new StreamingTextResponse(stream);
   } catch (error) {
     console.error("API request error:", error);
     throw error;
   }
-};
-
-// https://api-inference.huggingface.co/models/OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5
+}
